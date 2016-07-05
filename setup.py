d4e4dac1644912835e11a142fde74e1058ce9cb0
Timothy Liu <chbrahmsliu@gmail.com>
2016-07-05 19:35:53 -0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'xpdAn',
+    version = '0.0.1',
+    packages = find_packages(),
+    description = 'analysis module',
+    zip_safe = False,
+    url = 'http:/github.com/chiahaoliu/xpdAn'
+    )

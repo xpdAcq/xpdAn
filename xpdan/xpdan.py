03f7459ab4ca5244169f90ed0502941a7ed16934
Timothy Liu <chbrahmsliu@gmail.com>
2016-07-10 17:04:03 -0400

    add pd.DataFrame method

--- xpdan/xpdan.py
+++ xpdan/xpdan.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from databroker.databroker import DataBroker as db
 
 class xpdAn:
@@ -6,19 +7,20 @@ class xpdAn:
 
     _default_dict = {'group':'XPD'}
 
-    def __init__(self, saf_num, is_prun=True, is_setup=False):
+    def __init__(self, saf_num, current_search=None, is_prun=True, is_setup=False):
         print("====Analysis calss has been instantiated====")
-        #self._saf_num = saf_num
         self.saf_num = self.update_saf(saf_num)
+        self._current_search = current_search
+        #FIXME - add hook to is_prun, is_setup filter
 
     def update_saf(self, saf_num):
-        """ update saf """
+        """ update saf and pull out all headers belong to this saf """
         self.saf_num = saf_num
-        self.headers = self._all_headers()
+        self.all_headers = self._all_headers()
 
 
     def _all_headers(self):
-        """ methods that pull out all headers"""
+        """ methods that pull out all headers """
         self._default_dict.update(dict(bt_safN=self.saf_num))
         headers = db(**self._default_dict)
         self._default_dict.popitem()
@@ -34,20 +36,22 @@ class xpdAn:
         return headers
 
 
-    #@property
-    #def saf_num(self):
-    #    return self._saf_num
+    @property
+    def current_search(self):
+        return self._current_search
 
-    #@saf_num.setter
-    #def saf_num(self, val):
-    #    self._saf_num = str(val)
-    #    self.headers = self._all_headers()
+
+    def _set_current_search(self, headers):
+        print("INFO: attribute of `current search` has been updated.")
+        self._current_search = headers
+
 
     def list(self, *, group_index=None, **kwargs):
         """ method that lists/filter headers """
         _default_key = 'sa_name' # default grouping
         if not kwargs:
             group_list = self._filter(_default_key)
+            self._set_current_search(group_list)
         else:
             if len(kwargs) != 1:
                 print("WARNING: we only allows one search key now")
@@ -55,10 +59,11 @@ class xpdAn:
                 key = list(kwargs.keys()).pop()
                 val = list(kwargs.values()).pop()
                 group_list = self._filter(key, val)
-                self._search_rv = group_list
+                self._set_current_search(group_list)
             else:
                 print('WARNING: oppps')
         #FIXME add group_index method
+
 
     def _filter(self, key, value=None):
         """ assume a flat md_dict so everything is in 'start' """
@@ -69,18 +74,18 @@ class xpdAn:
             for h in self.headers:
                 if key in h.start:
                     full_list.append(h.start[key])
-                    unique_list = list(set(full_list))
+                else:
+                    full_list.append('missing sample name')
+                unique_list = list(set(full_list))
             group_list = []
             for el in unique_list:
-                search_dict = self._default_dict.update({key:el})
                 h_list = [h for h in self.headers if h.start[key] == el]
-                #h_list = db(**search_dict)
                 group_list.append(h_list)
-            print("Required field: {} resulting in following grouping:\n"
+            print("Required field: {} resulting in following grouping:"
                   .format(key))
-            for ind, el in enumerate(group_list):
-                print("{}  has  {}  headers under current"
-                      "SAF number".format(unique_list[ind], len(el)))
+            pd_table =self.table_gen([group_list,list(map(len,group_list))],
+                                     row_name=unique_list,
+                                     col_name=[key, '# of headers'])
             print("INFO: to subsample this list, please gives the key value"
                   " and do `an.list(key=value)` again")
         else:
@@ -88,7 +93,11 @@ class xpdAn:
             group_list = [h for h in self.headers if h.start.get(key) == value]
             print("INFO: search with '{} = {}' has pulled out {}"
                   "headers".format(len(group_list)))
-
         return group_list
 
 
+    def table_gen(self, data, row_name=None, col_name=None):
+        """ thin layer of pd.dataFrame to include print """
+        pd_table = pd.DataFrame(data, row_name, col_name)
+        print(pd_table)
+        return pd_table

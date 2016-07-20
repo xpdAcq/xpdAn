1acc59fb1d6f70f04e5196de4a59b00e23b8eb38
chiahaoliu <chbrahmsliu@gmail.com>
2016-07-20 19:20:02 -0400

    secure commit

--- xpdan/qt_search_v2.py
+++ xpdan/qt_search_v2.py
@@ -5,20 +5,18 @@ class XpdanSerch(QtGui.QWidget):
 
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("xpdAn search")
-        self.setGeometry(1024, 1024, 400, 300)
 
         self.initUI()
 
     def initUI(self):
+        # option_list
+        self.field_list = ['bt_piLast', 'sa_name', 'ex_name', 'sp_type']
+        self.field_option_list = []
+
         #generate btn
         generate_btn = QtGui.QPushButton("generate query", self)
         generate_btn.clicked.connect(self.generate)
         generate_btn.resize(generate_btn.sizeHint())
-        # reset btn
-        reset_btn = QtGui.QPushButton("reset query", self)
-        reset_btn.clicked.connect(self.reset)
-        reset_btn.resize(reset_btn.sizeHint())
 
         # set up fileds
         self.f1 = QtGui.QLabel('PI last name')
@@ -26,25 +24,30 @@ class XpdanSerch(QtGui.QWidget):
         self.f1_cbox = QtGui.QComboBox(self)
         self.f1_cbox.addItem("and")
         self.f1_cbox.addItem("or")
+        self.field_option_list.append((self.f1_Edit, self.f1_cbox))
+
+        #self.f1_cbox.activated[str].connect(self.f1_and_or_option)
 
         self.f2 = QtGui.QLabel('sample name')
         self.f2_Edit = QtGui.QLineEdit()
         self.f2_cbox = QtGui.QComboBox(self)
         self.f2_cbox.addItem("and")
         self.f2_cbox.addItem("or")
+        self.field_option_list.append((self.f2_Edit, self.f2_cbox))
 
         self.f3 = QtGui.QLabel('experiment name')
         self.f3_Edit = QtGui.QLineEdit()
         self.f3_cbox = QtGui.QComboBox(self)
         self.f3_cbox.addItem("and")
         self.f3_cbox.addItem("or")
+        self.field_option_list.append((self.f3_Edit, self.f3_cbox))
 
         self.f4 = QtGui.QLabel('scan type')
         self.f4_Edit = QtGui.QLineEdit()
         self.f4_cbox = QtGui.QComboBox(self)
         self.f4_cbox.addItem("and")
         self.f4_cbox.addItem("or")
-
+        self.field_option_list.append((self.f4_Edit, self.f4_cbox))
 
         grid = QtGui.QGridLayout()
         grid.setSpacing(5)
@@ -69,14 +72,17 @@ class XpdanSerch(QtGui.QWidget):
         hbox = QtGui.QHBoxLayout()
         hbox.addStretch(1)
         hbox.addWidget(generate_btn)
-        hbox.addWidget(reset_btn)
 
         vbox = QtGui.QVBoxLayout()
         vbox.addStretch(1)
-        vbox.addLayout(hbox)
         vbox.addLayout(grid)
+        vbox.addLayout(hbox)
+
 
         self.setLayout(vbox)
+
+        self.setWindowTitle("xpdAn search")
+        self.setGeometry(2048, 0, 400, 300)
 
         self.show()
 
@@ -84,12 +90,14 @@ class XpdanSerch(QtGui.QWidget):
     def generate(self):
         """ generate search query """
         print('Generate search query')
-        print('f1={}'.format(self.f1_Edit.text()))
-
-
-    def reset(self):
-        """ reset search query """
-        print('reset search query')
+        search_dict = {}
+        for i in range(len(self.field_list)):
+            key = self.field_list[i]
+            val = self.field_option_list[i][0].text()
+            option = self.field_option_list[i][1].currentText()
+            if val is not '':
+                search_dict.update({key:val})
+        print('search dict = {}'.format(search_dict))
 
 def main():
     app = QtGui.QApplication(sys.argv)

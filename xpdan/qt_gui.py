92c60c030028d91342939907a7159a039e89872f
chiahaoliu <chbrahmsliu@gmail.com>
2016-07-25 17:01:18 -0400

    finished first revist on gui framework

--- xpdan/qt_gui.py
+++ xpdan/qt_gui.py
@@ -1,4 +1,6 @@
 import sys
+import time
+import datetime
 from PyQt4 import QtGui, QtCore
 
 from .xpdan import XpdAn
@@ -6,26 +8,39 @@ an = XpdAn(saf_num=123) # dummy init
 
 class XpdanSearch(QtGui.QWidget):
 
+    FIELD_LIST = ['bt_piLast', 'sa_name','bt_experimenters',
+                  'plan_name']
+
+    MD_FIELD_LIST = ['bt_piLast', 'sa_name', 'sp_computed_exposure',
+                     'time', 'plan_name']
+
     def __init__(self):
         super().__init__()
         self._an = an # handing object
+
+        # option_list
+        self.field_option_list = []
+
+        # md field_list
+        self.md_field_list = []
+
         self.initUI()
 
     def initUI(self):
-        # option_list
-        self.field_list = ['bt_piLast', 'sa_name','bt_experimenters',
-                           'plan_name']
-        self.field_option_list = []
+
 
         #generate btn
         generate_btn = QtGui.QPushButton("generate query and search", self)
         generate_btn.clicked.connect(self.generate)
         generate_btn.resize(generate_btn.sizeHint())
+        #FIXME------ shortcut doesn't work
+        fireSearch = QtGui.QAction(self)
+        fireSearch.setShortcut('enter')
+        fireSearch.triggered.connect(self.generate)
 
-        # set up fileds
-
+        # set up search fileds
         self.f1 = QtGui.QLabel('PI last name')
-        self.f1_chkbox = QtGui.QCheckBox('+', self)
+        self.f1_chkbox = QtGui.QCheckBox(self)
         self.f1_Edit = QtGui.QLineEdit()
         self.f1_cbox = QtGui.QComboBox(self)
         self.f1_cbox.addItem("and")
@@ -35,7 +50,7 @@ class XpdanSearch(QtGui.QWidget):
         #self.f1_cbox.activated[str].connect(self.f1_and_or_option)
 
         self.f2 = QtGui.QLabel('sample name')
-        self.f2_chkbox = QtGui.QCheckBox('+', self)
+        self.f2_chkbox = QtGui.QCheckBox(self)
         self.f2_Edit = QtGui.QLineEdit()
         self.f2_cbox = QtGui.QComboBox(self)
         self.f2_cbox.addItem("and")
@@ -44,7 +59,7 @@ class XpdanSearch(QtGui.QWidget):
                                        self.f2_cbox))
 
         self.f3 = QtGui.QLabel('experimenter name')
-        self.f3_chkbox = QtGui.QCheckBox('+', self)
+        self.f3_chkbox = QtGui.QCheckBox(self)
         self.f3_Edit = QtGui.QLineEdit()
         self.f3_cbox = QtGui.QComboBox(self)
         self.f3_cbox.addItem("and")
@@ -53,7 +68,7 @@ class XpdanSearch(QtGui.QWidget):
                                        self.f3_cbox))
 
         self.f4 = QtGui.QLabel('scan type')
-        self.f4_chkbox = QtGui.QCheckBox('+', self)
+        self.f4_chkbox = QtGui.QCheckBox(self)
         self.f4_Edit = QtGui.QLineEdit()
         self.f4_cbox = QtGui.QComboBox(self)
         self.f4_cbox.addItem("and")
@@ -66,7 +81,7 @@ class XpdanSearch(QtGui.QWidget):
         self.output_box.setReadOnly(True)
         self.output_box.moveCursor(QtGui.QTextCursor.End)
         self.output_box.setLineWrapMode(self.output_box.NoWrap)
-        # this is default supported
+        # this is default supported in databroker
         #self.datakey = QtGui.QLabel('data key')
         #self.datakey_Edit = QtGui.QLineEdit()
         #self.datakey_cbox = QtGui.QComboBox(self)
@@ -76,30 +91,46 @@ class XpdanSearch(QtGui.QWidget):
         #self.field_option_list.append((self.datakey_Edit, self.datakey_cbox))
 
         self.date_range_option_list = []
-        self.start_date_chkbox = QtGui.QCheckBox('+', self)
+        self.start_date_chkbox = QtGui.QCheckBox(self)
         self.start_date_label = QtGui.QLabel('start date')
         self.start_date = QtGui.QCalendarWidget()
         self.start_date.setGridVisible(True)
         self.start_date_cbox = QtGui.QComboBox(self)
         self.start_date_cbox.addItem("and")
-        self.start_date_cbox.addItem("or")
+        #self.start_date_cbox.addItem("or")
         self.date_range_option_list.append((self.start_date_chkbox,
                                             self.start_date,
                                             self.start_date_cbox))
         #self.start_date.clicked[QtCore.QDate].connect(self.getDate)
 
-        self.end_date_chkbox = QtGui.QCheckBox('+', self)
-        self.end_date_label = QtGui.QLabel('stop date')
+        self.end_date_chkbox = QtGui.QCheckBox(self)
+        self.end_date_label = QtGui.QLabel('end date')
         self.end_date = QtGui.QCalendarWidget()
         self.end_date.setGridVisible(True)
         self.end_date_cbox = QtGui.QComboBox(self)
         self.end_date_cbox.addItem("and")
-        self.end_date_cbox.addItem("or")
+        #self.end_date_cbox.addItem("or")
         self.date_range_option_list.append((self.end_date_chkbox,
                                             self.end_date,
                                             self.end_date_cbox))
         #self.end_date.clicked[QtCore.QDate].connect(self.getDate)
 
+
+        # field to display md
+        #MD_FIELD_LIST = ['bt_piLast', 'sa_name', 'sp_computed_exposure',
+        #                 'plan_name', 'time']
+        self.md_chkbox_1 = QtGui.QCheckBox('PI last')
+        self.md_field_list.append(self.md_chkbox_1)
+        self.md_chkbox_2 = QtGui.QCheckBox('sample name' )
+        self.md_field_list.append(self.md_chkbox_2)
+        self.md_chkbox_3 = QtGui.QCheckBox('total exposure time')
+        self.md_field_list.append(self.md_chkbox_3)
+        self.md_chkbox_4 = QtGui.QCheckBox('plan name' )
+        self.md_field_list.append(self.md_chkbox_4)
+        self.md_chkbox_5 = QtGui.QCheckBox('time' )
+        self.md_field_list.append(self.md_chkbox_5)
+
+        # group widgets
         grid = QtGui.QGridLayout()
         grid.setSpacing(5)
 
@@ -137,13 +168,19 @@ class XpdanSearch(QtGui.QWidget):
         #grid.addWidget(self.datakey_Edit, 5, 1)
         #grid.addWidget(self.datakey_cbox, 5, 2)
 
+        md_grid = QtGui.QGridLayout()
+        md_grid.setSpacing(5)
+        md_grid.addWidget(QtGui.QLabel('Metadata to display'),1,0)
+        for i in range(len(self.md_field_list)):
+            md_grid.addWidget(self.md_field_list[i], 2, i)
+
         out_grid = QtGui.QGridLayout()
         out_grid.setSpacing(5)
 
         out_grid.addWidget(self.output, 1, 0)
         out_grid.addWidget(self.output_box, 2, 0)
 
-        # layout
+        # configure layout
         hbox = QtGui.QHBoxLayout()
         hbox.addStretch(1)
         hbox.addWidget(generate_btn)
@@ -151,7 +188,7 @@ class XpdanSearch(QtGui.QWidget):
         vbox = QtGui.QVBoxLayout()
         vbox.addStretch(1)
         vbox.addLayout(grid)
-        #vbox.addLayout(date_grid)
+        vbox.addLayout(md_grid)
         vbox.addLayout(hbox)
         vbox.addLayout(out_grid)
 
@@ -165,8 +202,9 @@ class XpdanSearch(QtGui.QWidget):
     def getDate(self, date):
         print(date.toString())
 
-    def printDate(self, obj):
-        print(obj.selectedDate())
+    def date2timestamp(self, obj):
+        selected_date = obj.selectedDate().toPyDate()
+        return time.mktime(selected_date.timetuple())
 
     def generate(self):
         """ generate search query """
@@ -175,44 +213,55 @@ class XpdanSearch(QtGui.QWidget):
         or_list = [] # query purpose
         in_list = [] # ref
 
-        for i in range(len(self.date_range_option_list)):
-            self.printDate(self.date_range_option_list[i][1])
+        # date range
+        time_dict = {}
+        gtlt_dict = {}
+        if self.start_date_chkbox.isChecked():
+            start_time = self.date2timestamp(self.start_date)
+            gtlt_dict.update({'$gte':start_time})
+        else:
+            pass
+        if self.end_date_chkbox.isChecked():
+            end_time = self.date2timestamp(self.end_date)
+            gtlt_dict.update({'$lte':end_time})
+        else:
+            pass
+        if gtlt_dict:
+            time_dict.update({'time':gtlt_dict})
+            search_dict.update(time_dict)
 
-        for i in range(len(self.field_list)):
-            key = self.field_list[i]
-            val = self.field_option_list[i][0].text().split(',')
+        # key-val search
+        for i in range(len(self.FIELD_LIST)):
+            key = self.FIELD_LIST[i]
+            y_n = self.field_option_list[i][0]
+            val = self.field_option_list[i][1].text().split(',')
             option = self.field_option_list[i][2].currentText()
-            if val != ['']:
-                if len(val) ==1:
+            if y_n.isChecked():
+                if len(val) ==1 and val != ['']:
                     _val = val[0].strip()
                     if option == 'or':
                         or_list.append({key:_val})
-                    #elif option == 'in':
-                    #    in_def = '{}.{}'.format(key, _val)
-                    #    search_dict.update({in_def :{'$exists':True}})
                     else: # and-logic
                         search_dict.update({key:val[0].strip()})
-                else:
+                elif len(val) >1:
                     for el in val:
                         _val = el.strip()
-                        #if option == 'in':
-                        #    in_def = '{}.{}'.format(key, _val)
-                        #    search_dict.update({in_def :{'$exist':True}})
-                        #else: # or-logic here. and-logic is excluded
                         or_list.append({key:_val})
-            #if option == 'or':
-            #    or_list.append({key:val})
-            #if option == 'in':
-            #    in_def = '{0}.{1}'.format(key, val)
-            #    search_dict.update({in_def :{'$exist':True}})
+                else:
+                    # + is selected but no field entered, skipped
+                    pass
+
+        # update logic
         if or_list:
             search_dict.update({'$or':or_list})
-        #print('search dict = {}'.format(search_dict))
+
+        # validate search key
         if len(search_dict) < 2:
-            #print('Warning: at least need two fields')
             self.output_box.insertPlainText("Warning: at least need one"
                                             " key\n")
             return
+
+        # show query history
         self.output_box.insertPlainText('{}\n'.format(search_dict))
         sb = self.output_box.verticalScrollBar()
         sb.setValue(sb.maximum())
@@ -224,6 +273,13 @@ class XpdanSearch(QtGui.QWidget):
 
         # operate on XpdAn
         self._an.search_dict = search_dict
+        md_field = []
+        for i in range(len(self.md_field_list)):
+            el = self.md_field_list[i]
+            if el.isChecked():
+                md_field.append(self.MD_FIELD_LIST[i])
+        print(md_field)
+        self._an.header_md_fields
         self._an.list()
 """
 def main():

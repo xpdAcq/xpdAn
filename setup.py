b55309bdc0d52255feb60c8ec28238e73bf35cf4
christopher <cjwright4242@gmail.com>
2016-09-20 11:23:07 -0400

    BLD: fix travis stuff

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='xpdan',
-    version='0.0.1',
+    version='0.0.0-alpha',
     packages=find_packages(),
     description='data processing module',
     zip_safe=False,

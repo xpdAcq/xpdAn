f1e7dfbbd87dc671bb89ce12eac7d55eae475db7
christopher <cjwright4242@gmail.com>
2016-09-26 14:40:06 -0400

    TST: add smoke tests for sum logic

--- xpdan/tests/test_data_reduction.py
+++ xpdan/tests/test_data_reduction.py
@@ -1,5 +1,14 @@
-from xpdan.data_reduction import integrate
+from xpdan.data_reduction import integrate, sum_images
 
 
 def test_integrate_smoke(exp_db, handler):
     integrate(exp_db[-1], handler=handler)
+
+
+def test_sum_logic_smoke(exp_db, handler):
+    a = sum_images(exp_db[-1])
+    assert len(a) == 1
+    a = sum_images(exp_db[-1], [1, 2, 3])
+    assert len(a) == 1
+    a = sum_images(exp_db[-1], [(1, 3)])
+    assert len(a) == 1

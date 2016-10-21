b549bd8f967e025dfe922ffede46156aeddc1b3a
christopher <cjwright4242@gmail.com>
2016-10-21 11:45:49 -0400

    TST: support random image sizes

--- xpdan/tests/conftest.py
+++ xpdan/tests/conftest.py
@@ -18,6 +18,10 @@ import numpy as np
 if sys.version_info >= (3, 0):
     pass
 
+@pytest.fixture(scope='module')
+def img_size():
+    yield tuple(np.random.random_integers(0, 200, (2)))
+
 
 @pytest.fixture(scope='module')
 def mk_glbl():
@@ -46,12 +50,12 @@ def handler(exp_db):
 
 
 @pytest.fixture(scope='module')
-def exp_db(db, mk_glbl):
+def exp_db(db, mk_glbl, img_size):
     glbl = mk_glbl
     db2 = db
     mds = db2.mds
     fs = db2.fs
-    insert_imgs(mds, fs, 5, (200, 200), glbl.base)
+    insert_imgs(mds, fs, 5, img_size, glbl.base)
     yield db2
     print("DROPPING MDS")
     mds._connection.drop_database(mds.config['database'])
@@ -60,8 +64,8 @@ def exp_db(db, mk_glbl):
 
 
 @pytest.fixture(scope='module')
-def disk_mask(mk_glbl):
-    mask = np.random.random_integers(0, 1, (200, 200)).astype(bool)
+def disk_mask(mk_glbl, img_size):
+    mask = np.random.random_integers(0, 1, img_size).astype(bool)
     dirn = mk_glbl.base
     file_name = os.path.join(dirn, 'mask_test' + '.msk')
     assert ~os.path.exists(file_name)

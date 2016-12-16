faed5ccad8b88fced9eae1960ea0237aab3a6f15
christopher <cjwright4242@gmail.com>
2016-12-16 00:14:45 -0500

    TST: add beamtime_uid

--- xpdan/tests/utils.py
+++ xpdan/tests/utils.py
@@ -37,11 +37,13 @@ def insert_imgs(mds, fs, n, shape, save_dir=tempfile.mkdtemp(),
     -------
 
     """
+    beamtime_uid = str(uuid4())
     # Insert the dark images
     dark_img = np.ones(shape)
     dark_uid = str(uuid4())
     run_start = mds.insert_run_start(uid=str(uuid4()), time=time.time(),
                                      name='test-dark', dark_uid=dark_uid,
+                                     beamtime_uid=beamtime_uid,
                                      is_dark_img=True, **kwargs)
     data_keys = {
         'img': dict(source='testing', external='FILESTORE:',
@@ -71,6 +73,7 @@ def insert_imgs(mds, fs, n, shape, save_dir=tempfile.mkdtemp(),
     imgs = [np.ones(shape)] * n
     run_start = mds.insert_run_start(uid=str(uuid4()), time=time.time(),
                                      name='test', dark_uid=dark_uid,
+                                     beamtime_uid=beamtime_uid,
                                      **kwargs)
     data_keys = {
         'pe1_image': dict(source='testing', external='FILESTORE:',

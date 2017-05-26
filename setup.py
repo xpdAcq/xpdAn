d92a3894a0e21d3766902a56df7fe29590217f11
Christopher J. Wright <cjwright4242gh@gmail.com>
2017-05-26 11:13:15 -0400

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='xpdan',
-    version='0.0.0-alpha',
+    version='0.1.2',
     packages=find_packages(),
     description='data processing module',
     zip_safe=False,

1a3f493028dc8421a08e485d82b521c2800d91f0
christopher <cjwright4242@gmail.com>
2017-08-24 14:43:40 -0400

    WIP: reduction_core tests mirror reduction tests

--- xpdan/tests/test_data_reduction.py
+++ xpdan/tests/test_data_reduction.py
@@ -18,11 +18,21 @@ from xpdan.data_reduction import (integrate_and_save, integrate_and_save_last,
                                   save_tiff, save_last_tiff)
 
 
-def test_integrate_smoke(exp_db, fast_tmp_dir):
+def test_integrate_core_smoke(exp_db, fast_tmp_dir):
     old_files = os.listdir(fast_tmp_dir)
     old_times = [os.path.getmtime(os.path.join(fast_tmp_dir, f)) for f in
                  os.listdir(fast_tmp_dir)]
     integrate_and_save(exp_db[-1], db=exp_db, save_dir=fast_tmp_dir)
+    assert (set(old_files) != set(os.listdir(fast_tmp_dir)) or set(
+        old_times) != set(
+        [os.path.getmtime(os.path.join(fast_tmp_dir, f)) for f in
+         os.listdir(fast_tmp_dir)]))
+
+
+def test_integrate_last_core_smoke(exp_db, fast_tmp_dir):
+    old_files = os.listdir(fast_tmp_dir)
+    old_times = [os.path.getmtime(os.path.join(fast_tmp_dir, f)) for f in
+                 os.listdir(fast_tmp_dir)]
     integrate_and_save_last(db=exp_db, save_dir=fast_tmp_dir)
     assert (set(old_files) != set(os.listdir(fast_tmp_dir)) or set(
         old_times) != set(
@@ -30,11 +40,23 @@ def test_integrate_smoke(exp_db, fast_tmp_dir):
          os.listdir(fast_tmp_dir)]))
 
 
-def test_save_tiff_smoke(exp_db, fast_tmp_dir):
+def test_save_tiff_core_smoke(exp_db, fast_tmp_dir):
     old_files = os.listdir(fast_tmp_dir)
     old_times = [os.path.getmtime(os.path.join(fast_tmp_dir, f)) for f in
                  os.listdir(fast_tmp_dir)]
     save_tiff(exp_db[-1], db=exp_db, save_dir=fast_tmp_dir)
+
+    assert (
+        set(old_files) != set(os.listdir(fast_tmp_dir)) or set(
+            old_times) != set(
+            [os.path.getmtime(os.path.join(fast_tmp_dir, f)) for f in
+             os.listdir(fast_tmp_dir)]))
+
+
+def test_save_last_tiff_core_smoke(exp_db, fast_tmp_dir):
+    old_files = os.listdir(fast_tmp_dir)
+    old_times = [os.path.getmtime(os.path.join(fast_tmp_dir, f)) for f in
+                 os.listdir(fast_tmp_dir)]
     save_last_tiff(db=exp_db, save_dir=fast_tmp_dir)
 
     assert (

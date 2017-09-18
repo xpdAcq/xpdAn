28e931c0f30103cf6521bfe37cc2df210048d41a
christopher <cjwright4242@gmail.com>
2017-09-18 16:36:57 -0400

    WIP: get ready for tag

--- setup.py
+++ setup.py
@@ -1,8 +1,21 @@
+import ast
 from setuptools import setup, find_packages
+
+
+def find_version(filename):
+    with open(filename) as f:
+        initlines = f.readlines()
+    version_line = None
+    for line in initlines:
+        if line.startswith('__version__'):
+            vstr = line.strip().split()[-1]
+            ver = ast.literal_eval(vstr)
+            break
+
 
 setup(
     name='xpdan',
-    version='0.0.0-alpha',
+    version=find_version('xpdan/__init__.py'),
     packages=find_packages(),
     description='data processing module',
     zip_safe=False,

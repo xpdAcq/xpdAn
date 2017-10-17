eab7fbf066f98ca47179a5014b2f4cc67808b3fa
eculbertson <eac2240@columbia.edu>
2017-10-17 13:08:55 -0400

    Added function get_filename_prefix, calls get_filename_prefix in render_and_clean

--- xpdan/formatters.py
+++ xpdan/formatters.py
@@ -3,10 +3,25 @@ import string
 from collections import defaultdict
 from pathlib import Path
 
+base_template = (''
+                 '{folder_tag_stuff}/'
+                 '{analyzed_start[analysis_stage]}/'
+                 '{raw_start[sample_name]}_'
+                 '{human_timestamp}_'
+                 '[temp={raw_event[data][temperature]:1.2f}'
+                 '{raw_descriptor[data_keys][temperature][units]}]_'
+                 '[dx={raw_event[data][diff_x]:1.3f}'
+                 '{raw_descriptor[data_keys][diff_x][units]}]_'
+                 '[dy={raw_event[data][diff_y]:1.3f}'
+                 '{raw_descriptor[data_keys][diff_y][units]}]_'
+                 '{raw_start[uid]:.6}_'
+                 '{raw_event[seq_num]:03d}{ext}')
+
 
 class PartialFormatter(string.Formatter):
     def get_field(self, field_name, args, kwargs):
         # Handle a key not found
+        # Expects the return value to be a tuple of (obj,used_key)
         try:
             val = super(PartialFormatter, self).get_field(field_name, args,
                                                           kwargs)
@@ -55,6 +70,7 @@ pfmt = PartialFormatter()
 def clean_template(template, removals=None, cfmt=cfmt):
     if removals is None:
         removals = ['temp', 'dx', 'dy']
+    # this will essentially replace any nonexistent keys (field names) with ''
     d = cfmt.format(template, defaultdict(str))
 
     for r in removals:
@@ -70,7 +86,28 @@ def clean_template(template, removals=None, cfmt=cfmt):
     print('saving file at {}'.format(f))
     return f
 
+# Replaces each parameter name within folder_tag_list with the value of that parameter.
+# 
+def get_filename_prefix(folder_tag_list,md):
+    result =''
+    for tag in folder_tag_list:
+        if isinstance(tag,str):
+            addition = str(md[tag])
+        else:
+            sub_md = md.copy()
+            for item in tag:
+                sub_md = sub_md[item]
+            addition = str(sub_md) 
+        result += addition + '/'
+    return result
+
 
 def render_and_clean(string, formatter=pfmt, **kwargs):
+    try:
+        folder_tag_list = kwargs['folder_tag_list']
+    except(KeyError):
+        folder_tag_list = []
+    filename_prefix = get_filename_prefix(folder_tag_list, kwargs)
+    # format replaces curly braces with the value in kwargs associated with the key in curly braces
     formatted_string = formatter.format(string, **kwargs)
-    return clean_template(formatted_string)
+    return clean_template(filename_prefix + formatted_string)

1773e6d3e20b1a95bcea344cdbfb1532aa9675e3
christopher <cjwright4242@gmail.com>
2017-11-24 14:08:18 -0500

    ENH: add more rever activities

--- setup.py
+++ setup.py
@@ -1,21 +1,9 @@
-import ast
 from setuptools import setup, find_packages
 
 
-def find_version(filename):
-    with open(filename) as f:
-        initlines = f.readlines()
-    version_line = None
-    for line in initlines:
-        if line.startswith('__version__'):
-            vstr = line.strip().split()[-1]
-            ver = ast.literal_eval(vstr)
-            break
-    return ver
-
 setup(
     name='xpdan',
-    version=find_version('xpdan/__init__.py'),
+    version='0.2.0',
     packages=find_packages(),
     description='data processing module',
     zip_safe=False,

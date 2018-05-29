fc4aabd733d83326aa60126ccef6bfc9369b1228
christopher <cjwright4242@gmail.com>
2018-05-28 20:57:17 -0400

    bumped version to 0.3.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='xpdan',
-    version='0.2.3rc',
+    version='0.3.0',
     packages=find_packages(),
     description='data processing module',
     zip_safe=False,

227983379524178bff535e2b07e92b5250281a6e
christopher <cjwright4242@gmail.com>
2018-06-01 02:38:38 -0400

    bumped version to 0.3.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='xpdan',
-    version='0.3.2',
+    version='0.3.3',
     packages=find_packages(),
     description='data processing module',
     zip_safe=False,

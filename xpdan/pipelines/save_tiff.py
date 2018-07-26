c7cb76117e5f6c80ce008ea6b1f5ade88c1b1094
christopher <cjwright4242@gmail.com>
2018-07-26 14:38:38 -0400

    add save tiff pipeline

--- xpdan/pipelines/save_tiff.py
+++ xpdan/pipelines/save_tiff.py
@@ -1,185 +1,157 @@
-"""Save tiff pipeline"""
 import os
-from operator import sub
 
-import shed.event_streams as es
-import tifffile
-from shed.event_streams import star
-
-from bluesky.callbacks.broker import LiveImage
-from streamz import Stream
-from xpdan.db_utils import query_dark, temporal_prox
-from xpdan.dev_utils import _timestampstr
-from xpdan.formatters import render_and_clean
+import numpy as np
+from shed.translation import FromEventStream
+from streamz_ext import Stream
+from tifffile import imsave
+from xpdan.callbacks import StartStopCallback
+from xpdan.db_utils import query_background, query_dark, temporal_prox
+from xpdan.formatters import render, clean_template
 from xpdan.io import dump_yml
-from xpdan.pipelines.pipeline_utils import (if_dark, base_template)
+from xpdan.pipelines.pipeline_utils import (_timestampstr,
+                                            Filler,
+                                            base_template)
+from xpdconf.conf import glbl_dict
+from xpdtools.pipelines.raw_pipeline import (
+    raw_background_dark,
+    raw_foreground_dark, img_counter,
+    raw_foreground,
+    dark_corrected_foreground,
+    raw_background)
 
-_s = set()
+image_name = glbl_dict['image_field']
+db = glbl_dict['exp_db']
+calibration_md_folder = {'folder': 'xpdAcq_calib_info.yml'}
 
+filler = Filler(db=db)
+# Build the general pipeline from the raw_pipeline
+raw_source = Stream(stream_name='raw source')
 
-# TODO: refactor templating
-def conf_save_tiff_pipeline(db, save_dir, *, write_to_disk=False, vis=True,
-                            image_data_key='pe1_image'):
-    """Total data processing pipeline for XPD
+# TODO: change this when new dark logic comes
+# Check that the data isn't a dark
+dk_uid = (FromEventStream('start', (), upstream=raw_source)
+          .map(lambda x: 'sc_dk_field_uid' in x))
+# Fill the raw event stream
+source = (raw_source
+          .combine_latest(dk_uid)
+          .filter(lambda x: x[1])
+          .pluck(0)
+          # Filler returns None for resource/datum data
+          .starmap(filler).filter(lambda x: x is not None))
+# Get all the documents
+start_docs = FromEventStream('start', (), source)
+descriptor_docs = FromEventStream('descriptor', (), source,
+                                  event_stream_name='primary')
+event_docs = FromEventStream('event', (), source, event_stream_name='primary')
+all_docs = (event_docs
+            .combine_latest(start_docs, descriptor_docs, emit_on=0)
+            .starmap(lambda e, s, d: {'raw_event': e, 'raw_start': s,
+                                      'raw_descriptor': d,
+                                      'human_timestamp': _timestampstr(
+                                          s['time'])}))
 
-    Parameters
-    ----------
-    db: databroker.broker.Broker instance
-        The databroker holding the data, this must be specified as a `db=` in
-        the function call (keyword only argument)
-    write_to_disk: bool, optional
-        If True write files to disk, defaults to False
-    save_dir: str
-        The folder in which to save the data, this must be specified as a
-        `save_dir=` in the function call (keyword only argument)
-    vis: bool, optional
-        If True visualize the data. Defaults to False
-    image_data_key: str, optional
-        The key for the image data, defaults to `pe1_image`
+# If new calibration uid invalidate our current calibration cache
+start_timestamp = FromEventStream('start', ('time',), source)
 
-    Returns
-    -------
-    source: Stream
-        The source for the graph
+# Clean out the cached darks and backgrounds on start
+# so that this will run regardless of background/dark status
+# note that we get the proper data (if it exists downstream)
+start_docs.sink(lambda x: raw_background_dark.emit(0.0))
+start_docs.sink(lambda x: raw_background.emit(0.0))
+start_docs.sink(lambda x: raw_foreground_dark.emit(0.0))
 
-    See also
-    --------
-    xpdan.tools.mask_img
-    """
-    print('start pipeline configuration')
-    light_template = os.path.join(save_dir, base_template)
-    raw_source = Stream(stream_name='Raw Data')  # raw data
-    source = es.fill_events(db, raw_source)  # filled raw data
+bg_query = (start_docs.map(query_background, db=db))
+bg_docs = (bg_query
+           .zip(start_docs)
+           .starmap(temporal_prox)
+           .filter(lambda x: x != [])
+           .map(lambda x: x[0].documents(fill=True))
+           .flatten())
 
-    # DARK PROCESSING
+# Get bg dark
+bg_dark_query = (FromEventStream('start', (), bg_docs)
+                 .map(query_dark, db=db)
+                 )
+(FromEventStream('event', ('data', image_name),
+                 bg_dark_query.map(lambda x: x[0].documents(fill=True))
+                 .flatten()).map(np.float32)
+ .connect(raw_background_dark))
 
-    # if not dark do dark subtraction
-    if_not_dark_stream = es.filter(lambda x: not if_dark(x), source,
-                                   input_info={0: ((), 0)},
-                                   document_name='start',
-                                   stream_name='If not dark',
-                                   full_event=True)
-    eventify_raw_start = es.Eventify(if_not_dark_stream,
-                                     stream_name='eventify raw start')
-    h_timestamp_stream = es.map(_timestampstr, if_not_dark_stream,
-                                input_info={0: 'time'},
-                                output_info=[('human_timestamp',
-                                              {'dtype': 'str'})],
-                                full_event=True,
-                                stream_name='human timestamp')
+# Get background
+(FromEventStream('event', ('data', image_name), bg_docs).map(np.float32)
+ .connect(raw_background))
 
-    # only the primary stream
-    if_not_dark_stream_primary = es.filter(lambda x: x[0]['name'] == 'primary',
-                                           if_not_dark_stream,
-                                           document_name='descriptor',
-                                           stream_name='Primary')
+# Get foreground dark
+fg_dark_query = (start_docs.map(query_dark, db=db))
+fg_dark_query.filter(lambda x: x != [] and isinstance(x, list)).sink(print)
+fg_dark_query.filter(lambda x: x == []).sink(lambda x: print('No dark found!'))
+(FromEventStream('event', ('data', image_name),
+                 fg_dark_query.filter(lambda x: x != [])
+                 .map(lambda x: x if not isinstance(x, list) else x[0])
+                 .map(lambda x: x.documents(fill=True)).flatten()
+                 ).map(np.float32)
+ .connect(raw_foreground_dark))
 
-    dark_query = es.Query(db,
-                          if_not_dark_stream,
-                          query_function=query_dark,
-                          query_decider=temporal_prox,
-                          stream_name='Query for FG Dark')
-    dark_query_results = es.QueryUnpacker(db, dark_query,
-                                          stream_name='Unpack FG Dark')
-    # Do the dark subtraction
-    zlid = es.zip_latest(if_not_dark_stream_primary,
-                         dark_query_results,
-                         stream_name='Combine darks and lights')
-    dark_sub_fg = es.map(sub,
-                         zlid,
-                         input_info={0: (image_data_key, 0),
-                                     1: (image_data_key, 1)},
-                         output_info=[('img', {'dtype': 'array',
-                                               'source': 'testing'})],
-                         md=dict(stream_name='Dark Subtracted Foreground',
-                                 analysis_stage='dark_sub'))
-    if vis:
-        dark_sub_fg.sink(star(LiveImage('img')))
+# Get foreground
+FromEventStream('event', ('seq_num',), source, stream_name='seq_num'
+                ).connect(img_counter)
+(FromEventStream('event', ('data', image_name), source, principle=True,
+                 stream_name='raw_foreground').map(np.float32)
+ .connect(raw_foreground))
 
-    if write_to_disk:
-        eventify_raw_descriptor = es.Eventify(
-            if_not_dark_stream, stream_name='eventify raw descriptor',
-            document='descriptor')
-        exts = ['.tiff']
-        eventify_input_streams = [dark_sub_fg]
-        input_infos = [
-            {'data': ('img', 0), 'file': ('filename', 1)},
-        ]
-        saver_kwargs = [{}]
-        eventifies = [es.Eventify(
-            s, stream_name='eventify {}'.format(s.stream_name)) for s in
-            eventify_input_streams]
+# Save out calibration data to special place
+h_timestamp = start_timestamp.map(_timestampstr)
 
-        mega_render = [
-            es.map(render_and_clean,
-                   es.zip_latest(
-                       es.zip(h_timestamp_stream,
-                              # human readable event timestamp
-                              if_not_dark_stream,  # raw events,
-                              stream_name='mega_render zip'
-                              ),
-                       eventify_raw_start,
-                       eventify_raw_descriptor,
-                       analysed_eventify
-                   ),
-                   string=light_template,
-                   input_info={
-                       'human_timestamp': (('data', 'human_timestamp'), 0),
-                       'raw_event': ((), 1),
-                       'raw_start': (('data',), 2),
-                       'raw_descriptor': (('data',), 3),
-                       'analyzed_start': (('data',), 4)
-                   },
-                   ext=ext,
-                   full_event=True,
-                   output_info=[('filename', {'dtype': 'str'})],
-                   stream_name='mega render '
-                               '{}'.format(analysed_eventify.stream_name)
-                   )
-            for ext, analysed_eventify in zip(exts, eventifies)]
-        streams_to_be_saved = [dark_sub_fg]
-        save_callables = [tifffile.imsave]
-        md_render = es.map(render_and_clean,
-                           eventify_raw_start,
-                           string=light_template,
-                           input_info={'raw_start': (('data',), 0), },
-                           output_info=[('filename', {'dtype': 'str'})],
-                           ext='.yml',
-                           full_event=True,
-                           stream_name='MD render')
+raw_source.starsink(StartStopCallback())
+# raw_source.visualize(os.path.expanduser('~/mystream.png'), source_node=True)
+# '''
+# SAVING
+# May rethink how we are doing the saving. If the saving was attached to the
+# translation nodes then it would be run before the rest of the graph was
+# processed.
 
-        make_dirs = [es.map(lambda x: os.makedirs(os.path.split(x)[0],
-                                                  exist_ok=True),
-                            cs,
-                            input_info={0: 'filename'},
-                            output_info=[('filename', {'dtype': 'str'})],
-                            stream_name='Make dirs {}'.format(cs.stream_name)
-                            ) for cs in mega_render]
+# This could be done by having each saver inside a callback which takes both
+# analyzed and raw documents, and creates the path from those two.
 
-        _s.update([es.map(writer_templater,
-                          es.zip_latest(
-                              es.zip(s1, s2, stream_name='zip render and data',
-                                     zip_type='truncate'), made_dir,
-                              stream_name='zl dirs and render and data'
-                              ),
-                          input_info=ii,
-                          output_info=[('final_filename', {'dtype': 'str'})],
-                          stream_name='Write {}'.format(s1.stream_name),
-                          **kwargs) for
-                   s1, s2, made_dir, ii, writer_templater, kwargs
-                   in
-                   zip(
-                       streams_to_be_saved,
-                       mega_render,
-                       make_dirs,  # prevent run condition btwn dirs and files
-                       input_infos,
-                       save_callables,
-                       saver_kwargs
-                   )])
+start_yaml_string = (start_docs.map(lambda s: {'raw_start': s,
+                                               'ext': '.yaml',
+                                               'analysis_stage': 'meta'
+                                               })
+                     .map(lambda kwargs, string, **kwargs2: render(string,
+                                                                   **kwargs,
+                                                                   **kwargs2),
+                          string=base_template,
+                          base_folder=glbl_dict['tiff_base'])
+                     )
+start_yaml_string.map(clean_template).zip(start_docs).starsink(dump_yml)
 
-        _s.add(es.map(dump_yml, es.zip(eventify_raw_start, md_render),
-                      input_info={0: (('data', 'filename'), 1),
-                                  1: (('data',), 0)},
-                      full_event=True,
-                      stream_name='dump yaml'))
-    return raw_source
+# create filename string
+filename_node = all_docs.map(
+    lambda kwargs, string, **kwargs2: render(string, **kwargs, **kwargs2),
+    string=base_template,
+    stream_name='base path',
+    base_folder=glbl_dict['tiff_base'])
+
+# SAVING NAMES
+filename_name_nodes = {}
+for name, analysis_stage, ext in zip(['dark_corrected_image_name'],
+                                     ['dark_sub'], ['.tiff']):
+    if ext:
+        temp_name_node = filename_node.map(render,
+                                           analysis_stage=analysis_stage,
+                                           ext=ext)
+    else:
+        temp_name_node = filename_node.map(render,
+                                           analysis_stage=analysis_stage)
+
+    filename_name_nodes[name] = temp_name_node.map(clean_template)
+    (filename_name_nodes[name].map(os.path.dirname)
+     .sink(os.makedirs, exist_ok=True, ))
+
+# dark corrected img
+(filename_name_nodes['dark_corrected_image_name']
+ .zip(dark_corrected_foreground)
+ .starsink(imsave, stream_name='dark corrected foreground'))
+
+save_kwargs = start_yaml_string.kwargs
+filename_node.kwargs = save_kwargs

4b38f4b6923be4ef2e4a9aba34c99efee49c30c1
christopher <cjwright4242@gmail.com>
2018-12-04 16:35:16 -0500

    better tests, more coverage

--- xpdan/tests/test_callbacks.py
+++ xpdan/tests/test_callbacks.py
@@ -10,10 +10,21 @@ def test_run_router(RE, hw):
         L.append(('start', start_doc))
         return lambda n, d: L.append((n, d))
 
-    rr = RunRouter([appender])
-    RE.subscribe(rr)
-    RE.subscribe(lambda n, d: LL.append((n, d)))
+    def not_interested(start_doc):
+        return
+
+    # Run with not interesteds
+    rr = RunRouter([not_interested, ])
+    rr_token = RE.subscribe(rr)
 
     RE(bp.count([hw.img], 1))
+
+    # now we're interested
+    RE.unsubscribe(rr_token)
+    rr = RunRouter([appender, not_interested])
+    rr_token = RE.subscribe(rr)
+    RE.subscribe(lambda n, d: LL.append((n, d)))
+    RE(bp.count([hw.img], 1))
+
 
     assert L == LL

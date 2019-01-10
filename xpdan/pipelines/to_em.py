13eb013c3c65a02ec829e68169e4e123199eb826
christopher <cjwright4242@gmail.com>
2019-01-10 15:44:13 -0500

    split analysis server into two functions

--- xpdan/pipelines/to_em.py
+++ xpdan/pipelines/to_em.py
@@ -20,9 +20,19 @@ def to_event_stream_no_ind(*nodes, publisher, **kwargs):
 
 
 def to_em(
-    dark_corrected_foreground, geometry, mask, mean, q, tth, pdf,
-        mean_max, q_at_mean_max, gr_max, r_at_gr_max, std,
-        **kwargs
+    dark_corrected_foreground,
+    geometry,
+    mask,
+    mean,
+    q,
+    tth,
+    pdf,
+    mean_max,
+    q_at_mean_max,
+    gr_max,
+    r_at_gr_max,
+    std,
+    **kwargs
 ):
     dark_corrected_tes = SimpleToEventStream(
         dark_corrected_foreground, ("img",), analysis_stage="dark_sub"
@@ -38,19 +48,24 @@ def to_em(
     integration_merge = mean.combine_latest(std, q, tth, emit_on=0)
     # TODO: stuff q/tth hints into start doc
     integration_tes = SimpleToEventStream(
-        integration_merge, ("mean", 'std', "q", "tth"),
-        analysis_stage="integration"
+        integration_merge,
+        ("mean", "std", "q", "tth"),
+        analysis_stage="integration",
     )
-    integration_tes.sink(print)
     # TODO: sq and fq TES
     # TODO: stuff q/r hints into start doc
-    pdf_tes = SimpleToEventStream(pdf, ("r", "gr", "config"), analysis_stage="pdf")
+    pdf_tes = SimpleToEventStream(
+        pdf, ("r", "gr", "config"), analysis_stage="pdf"
+    )
 
-    max_tes = SimpleToEventStream(mean_max.combine_latest(q_at_mean_max,
-                                                          emit_on=0),
-                                  ("iq_max", "q_iq_max"), analysis_stage="max")
-    max_pdf_tes = SimpleToEventStream(gr_max.combine_latest(r_at_gr_max,
-                                                          emit_on=0),
-                                  ("pdf_max", "r_pdf_max"),
-                                  analysis_stage="max_pdf")
+    max_tes = SimpleToEventStream(
+        mean_max.combine_latest(q_at_mean_max, emit_on=0),
+        ("iq_max", "q_iq_max"),
+        analysis_stage="max",
+    )
+    max_pdf_tes = SimpleToEventStream(
+        gr_max.combine_latest(r_at_gr_max, emit_on=0),
+        ("pdf_max", "r_pdf_max"),
+        analysis_stage="max_pdf",
+    )
     return locals()
--- xpdan/startup/analysis.py
+++ xpdan/startup/analysis.py
@@ -6,6 +6,7 @@ from xpdan.vend.callbacks.zmq import Publisher
 from xpdconf.conf import glbl_dict
 
 from xpdan.pipelines.main import pipeline_order
+from xpdtools.pipelines.extra import std_gen, z_score_gen
 from xpdtools.pipelines.qoi import max_intensity_mean, max_gr_mean
 from xpdan.pipelines.save import pipeline_order as save_pipeline_order
 from xpdan.pipelines.vis import vis_pipeline
@@ -50,23 +51,13 @@ def start_analysis(save=True, vis=True, **kwargs):
     d.start()
 
 
-def start_analysis_server(**kwargs):
-    d = RemoteDispatcher(
-        glbl_dict["outbound_proxy_address"],
-        # accept the raw data
-        prefix=b"raw",
-    )
-    install_qt_kicker(
-        loop=d.loop
-    )  # This may need to be d._loop depending on tag
-
-    order = pipeline_order + [max_intensity_mean, max_gr_mean] + [to_em]
+def create_analysis_pipeline(order, **kwargs):
     namespace = link(
         *order, raw_source=Stream(stream_name="raw source"), **kwargs
     )
     raw_source = namespace["raw_source"]
+
     # do inspection of pipeline for ToEventModel nodes, maybe?
-    #'''
     # for analyzed data with independent data (vis and save)
     an_with_ind_pub = Publisher(
         glbl_dict["inbound_proxy_address"], prefix=b"vis"
@@ -83,7 +74,7 @@ def start_analysis_server(**kwargs):
                     "integration_tes",
                     "pdf_tes",
                     "max_tes",
-                    "max_pdf_tes"
+                    "max_pdf_tes",
                 ]
             ],
             publisher=an_with_ind_pub
@@ -101,9 +92,9 @@ def start_analysis_server(**kwargs):
             *[
                 namespace[k]
                 for k in [
-                    #"dark_corrected_tes",
+                    # "dark_corrected_tes",
                     # "geometry_tes",
-                    #"mask_tes",
+                    # "mask_tes",
                     "integration_tes",
                     "pdf_tes",
                 ]
@@ -111,8 +102,29 @@ def start_analysis_server(**kwargs):
             publisher=an_no_ind_pub
         )
     )
-    #'''
+    return namespace
+
+
+def start_analysis_server(raw_source):
+    d = RemoteDispatcher(
+        glbl_dict["outbound_proxy_address"],
+        # accept the raw data
+        prefix=b"raw",
+    )
+    install_qt_kicker(
+        loop=d.loop
+    )
 
     d.subscribe(lambda *x: raw_source.emit(x))
     print("Starting Analysis Server")
     d.start()
+
+
+if __name__ == '__main__':  # pragma: no cover
+    order = (
+        pipeline_order
+        + [std_gen, z_score_gen, max_intensity_mean, max_gr_mean]
+        + [to_em]
+    )
+    namespace = create_analysis_pipeline(order, **kwargs)
+    start_analysis_server(namespace['raw_source'])

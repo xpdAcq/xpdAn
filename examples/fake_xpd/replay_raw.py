4ee3353e3e6f88bd56763a9b48159ffe5cde545a
christopher <cjwright4242@gmail.com>
2019-01-17 14:18:14 -0500

    WIP: get databroker server running

--- examples/fake_xpd/replay_raw.py
+++ examples/fake_xpd/replay_raw.py
@@ -12,7 +12,7 @@ db.prepare_hook = lambda x, y: copy.deepcopy(y)
 
 from xpdconf.conf import glbl_dict
 
-p = Publisher(glbl_dict["inbound_proxy_address"], prefix=b'raw')
+p = Publisher(glbl_dict["inbound_proxy_address"], prefix=b"raw")
 
 t0 = time.time()
 for hdr in list((db[-1],)):
@@ -21,24 +21,22 @@ for hdr in list((db[-1],)):
     for e in hdr.documents():
         if e[0] == "start":
             e[1].update(composition_string="EuTiO3")
-            e[1].update(
-                hints={'dimensions': [(['temperature'], 'primary')]})
-        if e[0] == 'descriptor':
-            e[1].update(
-                hints={'cs700': {'fields': ['temperature']}}
-            )
+            e[1].update(hints={"dimensions": [(["temperature"], "primary")]})
+            e[1].update(analysis_stage="raw")
+        if e[0] == "descriptor":
+            e[1].update(hints={"cs700": {"fields": ["temperature"]}})
         # if e[0] == "event":
         # if e[1]["seq_num"] > 3:
         #     break
-        if e[0] == 'resource':
-            if 'run_start' not in e[1]:
-                e[1].update(run_start=start['uid'])
+        if e[0] == "resource":
+            if "run_start" not in e[1]:
+                e[1].update(run_start=start["uid"])
         pprint(e[1])
         p(*e)
         input()
         # time.sleep(1)
-    #e = ("stop", stop)
-    #p(*e)
-    #time.sleep(1)
+    # e = ("stop", stop)
+    # p(*e)
+    # time.sleep(1)
 
 print(time.time() - t0)

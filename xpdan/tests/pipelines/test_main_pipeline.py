b3e9ed631aaa16d62cd88b6b7e02ef20488a68c0
Christopher J. Wright <cjwright4242@gmail.com>
2019-01-18 13:17:28 -0500

    consolidate tests

--- xpdan/tests/pipelines/test_main_pipeline.py
+++ xpdan/tests/pipelines/test_main_pipeline.py
@@ -9,6 +9,7 @@ from xpdan.pipelines.main import pipeline_order
 
 @pytest.mark.parametrize("exception", [True, False])
 @pytest.mark.parametrize("background", [True, False])
+@pytest.mark.parametrize("pe2", [True, False])
 def test_main_pipeline(
     exp_db,
     fast_tmp_dir,
@@ -17,6 +18,7 @@ def test_main_pipeline(
     start_uid2,
     background,
     exception,
+    pe2
 ):
     namespace = link(
         *pipeline_order, raw_source=Stream(stream_name="raw source"),
@@ -34,6 +36,8 @@ def test_main_pipeline(
     t0 = time.time()
     if background:
         uid = start_uid1
+    elif pe2:
+        uid = start_uid2
     else:
         uid = -1
     for nd in exp_db[uid].documents(fill=True):
@@ -59,65 +63,6 @@ def test_main_pipeline(
     assert iq_em.state == "stopped"
     destroy_pipeline(namespace["raw_source"])
     del namespace
-    limg.clear()
-    lbgc.clear()
-    lpdf.clear()
-
-
-def test_main_pipeline_pe2(
-    exp_db,
-    fast_tmp_dir,
-    start_uid3,
-    start_uid1,
-    start_uid2,
-    background=True,
-    exception=False,
-    pe2=True,
-):
-    namespace = link(
-        *pipeline_order, raw_source=Stream(stream_name="raw source"),
-        db=exp_db,
-    )
-    iq_em = ToEventStream(
-        namespace["mean"].combine_latest(namespace["q"], emit_on=0),
-        ("iq", "q"))
-    iq_em.sink(print)
-
-    limg = []
-    move_to_first(namespace["bg_corrected_img"].sink(lambda x: limg.append(x)))
-    lbgc = namespace["mean"].sink_to_list()
-    lpdf = namespace["iq_comp"].sink_to_list()
-    t0 = time.time()
-    if background:
-        uid = start_uid1
-    elif pe2:
-        uid = start_uid2
-    else:
-        uid = -1
-    for nd in exp_db[uid].documents():
-        name, doc = nd
-        if name == "start":
-            if exception:
-                doc["bt_wavelength"] = "bla"
-            nd = (name, doc)
-        try:
-            namespace["raw_source"].emit(nd)
-        except ValueError:
-            pass
-    t1 = time.time()
-    print(t1 - t0)
-    n_events = len(list(exp_db[-1].events()))
-    assert len(limg) == n_events
-    if exception:
-        assert_lbgc = 0
-    else:
-        assert_lbgc = n_events
-    assert len(lbgc) == assert_lbgc
-    assert len(lpdf) == assert_lbgc
-    assert iq_em.state == "stopped"
-    destroy_pipeline(namespace["raw_source"])
-    del namespace
-    del iq_em
     limg.clear()
     lbgc.clear()
     lpdf.clear()

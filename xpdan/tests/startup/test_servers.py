7e776e35c0b7ab9bfeb3c0781bddf188aad8c054
Christopher J. Wright <cjwright4242@gmail.com>
2019-01-24 21:06:25 -0500

    test db server

--- xpdan/tests/startup/test_servers.py
+++ xpdan/tests/startup/test_servers.py
@@ -10,16 +10,18 @@ from shed.simple import SimpleFromEventStream
 import bluesky.plans as bp
 from ophyd.sim import NumpySeqHandler
 from rapidz import Stream
+from xpdconf.conf import glbl_dict
 from xpdan.startup.portable_db_server import (
     run_server as portable_db_run_sever
 )
 from xpdan.startup.viz_server import run_server as viz_run_server
 from xpdan.startup.analysis_server import run_server as analysis_run_server
+from xpdan.startup.db_server import run_server as db_run_server
 from xpdan.vend.callbacks.core import Retrieve
 from xpdan.vend.callbacks.zmq import Publisher
 
 
-def test_protable_db_run_server(tmpdir, proxy, RE, hw):
+def test_portable_db_run_server(tmpdir, proxy, RE, hw):
     fn = str(tmpdir)
 
     def delayed_sigint(delay):  # pragma: no cover
@@ -153,3 +155,56 @@ def test_analysis_run_server(tmpdir, proxy, RE, hw):
         print("finished server")
     exp_proc.terminate()
     exp_proc.join()
+
+
+def test_db_run_server(tmpdir, proxy, RE, hw, db):
+    db.reg.handler_reg = {"NPY_SEQ": NumpySeqHandler}
+    glbl_dict['an_db'] = db
+    fn = str(tmpdir)
+
+    def delayed_sigint(delay):  # pragma: no cover
+        time.sleep(delay)
+        print("killing")
+        os.kill(os.getpid(), signal.SIGINT)
+
+    def run_exp(delay):  # pragma: no cover
+        time.sleep(delay)
+        print("running exp")
+
+        p = Publisher(proxy[0], prefix=b"raw")
+        RE.subscribe(p)
+
+        # Tiny fake pipeline
+        pp = Publisher(proxy[0], prefix=b"an")
+        raw_source = Stream()
+        SimpleFromEventStream(
+            "event",
+            ("data", "img"),
+            raw_source.starmap(Retrieve({"NPY_SEQ": NumpySeqHandler})),
+            principle=True,
+        ).map(lambda x: x * 2).SimpleToEventStream(
+            ("img2",), analysis_stage="pdf"
+        ).starsink(
+            pp
+        )
+        RE.subscribe(lambda *x: raw_source.emit(x))
+
+        RE(bp.count([hw.img], md=dict(analysis_stage="raw")))
+        print("finished exp")
+        p.close()
+
+    # Run experiment in another process (after delay)
+    exp_proc = multiprocessing.Process(target=run_exp, args=(2,), daemon=True)
+    exp_proc.start()
+
+    # send the message that will eventually kick us out of the server loop
+    threading.Thread(target=delayed_sigint, args=(10,)).start()
+    try:
+        print("running server")
+        db_run_server(fn)
+
+    except KeyboardInterrupt:
+        print("finished server")
+    exp_proc.terminate()
+    exp_proc.join()
+    assert db[-1].start['analysis_stage'] == 'pdf'

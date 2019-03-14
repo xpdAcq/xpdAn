b4189596fda37318b77fdafa69c070f7d343d5f8
Christopher J. Wright <cjwright4242@gmail.com>
2019-03-14 13:49:51 -0400

    bumped version to 0.6.1

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ entry_points = {
 
 setup(
     name="xpdan",
-    version='0.6.0',
+    version='0.6.1',
     packages=find_packages(),
     description="data processing module",
     zip_safe=False,

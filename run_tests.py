e1809b0d3c9e04fb275d9ef5226deacd59200dd5
Christopher J. Wright <cjwright4242@gmail.com>
2019-05-02 16:21:57 -0400

    try to run tests seperately

--- run_tests.py
+++ run_tests.py
@@ -3,18 +3,18 @@ import sys
 import pytest
 import os
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # show output results from every test function
-    args = ['-v', 'xpdan']
+    args = ["-vvxs"]
     # show the message output for skipped and expected failure tests
     if len(sys.argv) > 1:
         args.extend(sys.argv[1:])
-    print('pytest arguments: {}'.format(args))
+    print("pytest arguments: {}".format(args))
     # # compute coverage stats for xpdAcq
     # args.extend(['--cov', 'xpdAcq'])
     # call pytest and exit with the return code from pytest so that
     # travis will fail correctly if tests fail
-    os.environ['XPDAN_SETUP'] = str(1)
-    exit_res = pytest.main(args)
-    os.environ['XPDAN_SETUP'] = str(0)
+    for f in os.listdir("xpdan/tests"):
+        a = args.copy() + [f"xpdan/tests/{f}"]
+        exit_res = pytest.main(a)
     sys.exit(exit_res)

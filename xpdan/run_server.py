b2c768e79177fb22f0e11d5891a35c89a7440c47
st3107 <st3107@columbia.edu>
2020-09-17 14:14:46 -0400

    DOC: update run_server.py

--- xpdan/run_server.py
+++ xpdan/run_server.py
@@ -3,6 +3,16 @@ from bluesky.callbacks.zmq import RemoteDispatcher
 
 
 def run_server(server: RemoteDispatcher, name: str = "server"):
+    """Run the server.
+
+    Parameters
+    ----------
+    server :
+        The server.
+
+    name :
+        The name of the server to show in the log.
+    """
     try:
         print(" ".join(["Start", name, "..."]))
         server.start()
